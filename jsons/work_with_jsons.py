5b1a75296c51ae131aadbf71ee047d654330fa94
Vladislav Skibenko <vladislav.skibenko@fleetcor.com>
2023-06-24 17:02:29 +0300

    refactoring part 2

--- jsons/work_with_jsons.py
+++ jsons/work_with_jsons.py
@@ -12,7 +12,7 @@ async def word_mentor():
 
 def open_json_admins():
     if os.path.exists(JSON_FILE_ADMIN):
-        with open(JSON_FILE_ADMIN, 'r', encoding='utf-8') as file:
+        with open(JSON_FILE_ADMIN , 'r' , encoding='utf-8') as file:
             messages_admin = json.load(file)
     else:
         messages_admin = {}
@@ -21,13 +21,13 @@ def open_json_admins():
 
 def read_json_admin_file_add_user(user_id):
     if os.path.exists(JSON_FILE_ADMIN):
-        with open(JSON_FILE_ADMIN, 'r', encoding='utf-8') as file:
+        with open(JSON_FILE_ADMIN , 'r' , encoding='utf-8') as file:
             messages_admin = json.load(file)
 
         messages_admin["granted_users"].append(int(user_id))
 
-        with open(JSON_FILE_ADMIN, 'w', encoding='utf-8') as json_file:
-            json.dump(messages_admin, json_file, ensure_ascii=False, indent=4)
+        with open(JSON_FILE_ADMIN , 'w' , encoding='utf-8') as json_file:
+            json.dump(messages_admin , json_file , ensure_ascii=False , indent=4)
     else:
         print('Файла нет')
     messages = open_json_admins()
@@ -35,27 +35,27 @@ def read_json_admin_file_add_user(user_id):
 
 def read_json_admin_file_add_user_admin(user_id):
     if os.path.exists(JSON_FILE_ADMIN):
-        with open(JSON_FILE_ADMIN, 'r', encoding='utf-8') as file:
+        with open(JSON_FILE_ADMIN , 'r' , encoding='utf-8') as file:
             messages_admin = json.load(file)
 
         messages_admin["admins"].append(int(user_id))
 
-        with open(JSON_FILE_ADMIN, 'w', encoding='utf-8') as json_file:
-            json.dump(messages_admin, json_file, ensure_ascii=False, indent=4)
+        with open(JSON_FILE_ADMIN , 'w' , encoding='utf-8') as json_file:
+            json.dump(messages_admin , json_file , ensure_ascii=False , indent=4)
     else:
         print('Файла нет')
     messages = open_json_admins()
 
 
-def save_json_admins(role, data):     # roles_dict
+def save_json_admins(role , data):  # roles_dict
     if os.path.exists(JSON_FILE_ADMIN):
-        with open(JSON_FILE_ADMIN, 'r', encoding='utf-8') as file:
+        with open(JSON_FILE_ADMIN , 'r' , encoding='utf-8') as file:
             messages_admin = json.load(file)
 
-        messages_admin.get('roles_dict', {})[role] = data
+        messages_admin.get('roles_dict' , {})[role] = data
 
-        with open(JSON_FILE_ADMIN, 'w', encoding='utf-8') as json_file:
-            json.dump(messages_admin, json_file, ensure_ascii=False, indent=4)
+        with open(JSON_FILE_ADMIN , 'w' , encoding='utf-8') as json_file:
+            json.dump(messages_admin , json_file , ensure_ascii=False , indent=4)
 
         messages = open_json_admins()
     else:
@@ -64,11 +64,11 @@ def save_json_admins(role, data):     # roles_dict
 
 def del_json_user(user):
     if os.path.exists(JSON_FILE_ADMIN):
-        with open(JSON_FILE_ADMIN, 'r', encoding='utf-8') as file:
+        with open(JSON_FILE_ADMIN , 'r' , encoding='utf-8') as file:
             messages_admin = json.load(file)
         try:
 
-            del messages_admin.get('granted_users_is', {})[str(user)]
+            del messages_admin.get('granted_users_is' , {})[str(user)]
 
         except Exception as e:
             print(e)
@@ -76,7 +76,7 @@ def del_json_user(user):
 
         try:
 
-            messages_admin.get('granted_users', []).remove(int(user))
+            messages_admin.get('granted_users' , []).remove(int(user))
 
         except Exception as e:
             print(e)
@@ -93,7 +93,7 @@ def del_json_user(user):
 
 def del_json_admin(user):
     if os.path.exists(JSON_FILE_ADMIN):
-        with open(JSON_FILE_ADMIN, 'r', encoding='utf-8') as file:
+        with open(JSON_FILE_ADMIN , 'r' , encoding='utf-8') as file:
             messages_admin = json.load(file)
 
         try:
@@ -111,18 +111,18 @@ def del_json_admin(user):
         print('Файла нет')
 
 
-def add_granted_users_is_data(userid, name, surname, phone):
+def add_granted_users_is_data(userid , name , surname , phone):
     if os.path.exists(JSON_FILE_ADMIN):
-        with open(JSON_FILE_ADMIN, 'r', encoding='utf-8') as file:
+        with open(JSON_FILE_ADMIN , 'r' , encoding='utf-8') as file:
             messages_admin = json.load(file)
 
         try:
-            messages_admin.get('granted_users_is', {})[str(userid)] = {
-                "Name": str(name),
-                "Surname": str(surname),
+            messages_admin.get('granted_users_is' , {})[str(userid)] = {
+                "Name": str(name) ,
+                "Surname": str(surname) ,
                 "phone_number": str(phone)
             }
-            print(messages_admin.get('granted_users_is', {})[str(userid)])
+            print(messages_admin.get('granted_users_is' , {})[str(userid)])
         except Exception as e:
             print(e)
             pass
@@ -131,4 +131,3 @@ def add_granted_users_is_data(userid, name, surname, phone):
             json.dump(messages_admin , json_file , ensure_ascii=False , indent=4)
 
         messages = open_json_admins()
-
