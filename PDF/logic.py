ce6d286e609fc290a74fed40877eb6d4000e7e08
Vladislav Skibenko <vladislav.skibenko@fleetcor.com>
2023-07-11 09:49:42 +0300

    refactoring part 2

--- PDF/logic.py
+++ PDF/logic.py
@@ -60,14 +60,14 @@ def start_logic():
                 # tea_type=type
                 # name_of_tea=name_of_tea
                 # price_tea=price
-                print(color_type.get(row['Тип чая'], '#E75C21'), row['Наименование'], price, row['Тип чая'], row['Наименование'], price)
+                print(color_type.get(str(row['Тип чая']), '#E75C21'), row['Наименование'], price, str(row['Тип чая']), row['Наименование'], price)
                 # Здесь вставьте вызовы функций модулей horizon и square в соответствии с именем листа
                 if sheet.lower() == 'horizon':
-                    horizon_tag(color_type=color_type.get(row['Тип чая'].upper(), '#E75C21'), color_name='#FFFFFF',
-                                  color_price='#FFB12A', tea_type=row['Тип чая'], name_of_tea=row['Наименование'], price_tea=price)
+                    horizon_tag(color_type=color_type.get(str(row['Тип чая']).upper(), '#E75C21'), color_name='#FFFFFF',
+                                  color_price='#FFB12A', tea_type=str(row['Тип чая']), name_of_tea=row['Наименование'].replace('/', '\\'), price_tea=price)
                 elif sheet.lower() == 'square':
-                    square_tag(color_type=color_type.get(row['Тип чая'].upper(), '#E75C21'), color_name='#FFFFFF',
-                                 color_price='#FFB12A', tea_type=row['Тип чая'], name_of_tea=row['Наименование'], price_tea=price)
+                    square_tag(color_type=color_type.get(str(row['Тип чая']).upper(), '#E75C21'), color_name='#FFFFFF',
+                                 color_price='#FFB12A', tea_type=str(row['Тип чая']), name_of_tea=row['Наименование'].replace('/', '\\'), price_tea=price)
     except Exception as e:
         print(e)
 
