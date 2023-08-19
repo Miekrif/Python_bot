98cb7a706d2d3e9f196bc6d5594bd98cc6360089
Vladislav Skibenko <vladislav.skibenko@fleetcor.com>
2023-08-19 10:27:48 +0300

    init

--- fsm/close_work.py
+++ fsm/close_work.py
@@ -14,6 +14,8 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemo
 
 
 dp.middleware.setup(LoggingMiddleware())
+CHAT_ID_1 = "YOUR_CHAT_ID_1"
+CHAT_ID_2 = "YOUR_CHAT_ID_2"
 
 
 class Form(StatesGroup):
@@ -27,6 +29,8 @@ class Form(StatesGroup):
     FridgePhoto = State()  # холодильник
     ShiftSum = State()  # сумма смены
     TeaCeremony = State()  # кол-во людей на чайной церемонии
+    TeaCeremonyCount = State()
+    TeaCeremonyReason = State()
     Reason = State()  # причина 0 на чайной церемонии
 
 
@@ -152,6 +156,111 @@ async def handle_cleaning_photo(message: types.Message , state: FSMContext):
     await Form.CleaningPhotoType.set()
 
 
+# Обработка пункта 2 - Фото уборки:
+
+@dp.message_handler(lambda message: message.text == "Фото уборки" , state=Form.CleaningPhotoType)
+async def ask_cleaning_photo_type(message: types.Message):
+    markup = ReplyKeyboardMarkup(resize_keyboard=True , selective=True)
+    markup.add(KeyboardButton("Фотография раковины"))
+    markup.add(KeyboardButton("Пустая урна под баркой и в туалете"))
+    markup.add(KeyboardButton("Убранная летка"))
+    markup.add(KeyboardButton("Фото рубильников"))
+    markup.add(KeyboardButton("Фото работающего холодильника"))
+    markup.add(KeyboardButton("Отменить"))
+
+    await Form.CleaningPhoto.set()  # Устанавливаем состояние на ожидание фотографии уборки
+    await message.answer("Выберите, какое фото уборки вы хотите отправить:" , reply_markup=markup)
+
+
+# Далее вы можете создать обработчики для каждого из вариантов фотографий уборки.
+
+@dp.message_handler(lambda message: message.text == "Фотография раковины" , state=Form.CleaningPhoto)
+async def ask_sink_photo(message: types.Message):
+    await message.answer("Сделайте фото пустой раковины (вся посуда должна быть помыта, и зоны раковины).")
+
+
+# ... [добавьте обработчики для остальных фотографий уборки по аналогии]
+
+
+@dp.message_handler(lambda message: message.text == "Отменить" , state=Form.CleaningPhoto)
+async def cancel_cleaning_photo(message: types.Message , state: FSMContext):
+    await state.finish()
+    await message.answer("Вы отменили отправку фотографий уборки." , reply_markup=ReplyKeyboardRemove())
+
+
+@dp.message_handler(content_types=['photo'] , state=Form.CleaningPhoto)
+async def handle_cleaning_photo(message: types.Message , state: FSMContext):
+    # В данном контексте, мы просто сохраняем фото уборки. В реальном коде вы, возможно, захотите сохранять фотографии отдельно по типам.
+    await state.update_data(cleaning_photo=message.photo[-1].file_id)
+
+    markup = ReplyKeyboardMarkup(resize_keyboard=True , selective=True)
+    markup.add(KeyboardButton("Фотография чеков"))
+    markup.add(KeyboardButton("Фото уборки"))
+    markup.add(KeyboardButton("Готово"))
+
+    await message.answer("Фото уборки сохранено. Выберите следующий пункт или завершите отправку данных." ,
+                         reply_markup=markup)
+    await Form.CleaningPhotoType.set()
+
+
+@dp.message_handler(lambda message: message.text == "Сумма смены" , state=Form.CleaningPhotoType)
+async def ask_shift_sum(message: types.Message):
+    await message.answer("Введите сумму смены.")
+    await Form.ShiftSum.set()
+
+
+@dp.message_handler(state=Form.ShiftSum)
+async def handle_shift_sum(message: types.Message , state: FSMContext):
+    await state.update_data(shift_sum=message.text)
+
+    markup = ReplyKeyboardMarkup(resize_keyboard=True , selective=True)
+    markup.add(KeyboardButton("Фотография чеков"))
+    markup.add(KeyboardButton("Фото уборки"))
+    markup.add(KeyboardButton("Готово"))
+
+    await message.answer("Сумма смены сохранена. Выберите следующий пункт или завершите отправку данных." ,
+                         reply_markup=markup)
+    await Form.CleaningPhotoType.set()
+
+
+@dp.message_handler(lambda message: message.text == "Чайная церемония", state=Form.CleaningPhotoType)
+async def ask_tea_ceremony_count(message: types.Message):
+    await message.answer("Сколько людей записано на чайную церемонию?")
+    await Form.TeaCeremonyCount.set()
+
+
+@dp.message_handler(lambda message: message.text.isdigit(), state=Form.TeaCeremonyCount)
+async def handle_tea_ceremony_count(message: types.Message, state: FSMContext):
+    count = int(message.text)
+
+    await state.update_data(tea_ceremony_count=count)
+
+    if count == 0:
+        await message.answer("Почему никто не записан на чайную церемонию?")
+        await Form.TeaCeremonyReason.set()
+    else:
+        markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+        markup.add(KeyboardButton("Фотография чеков"))
+        markup.add(KeyboardButton("Фото уборки"))
+        markup.add(KeyboardButton("Готово"))
+
+        await message.answer("Количество людей сохранено. Выберите следующий пункт или завершите отправку данных.", reply_markup=markup)
+        await Form.CleaningPhotoType.set()
+
+
+@dp.message_handler(state=Form.TeaCeremonyReason)
+async def handle_tea_ceremony_reason(message: types.Message, state: FSMContext):
+    await state.update_data(tea_ceremony_reason=message.text)
+
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add(KeyboardButton("Фотография чеков"))
+    markup.add(KeyboardButton("Фото уборки"))
+    markup.add(KeyboardButton("Готово"))
+
+    await message.answer("Причина сохранена. Выберите следующий пункт или завершите отправку данных.", reply_markup=markup)
+    await Form.CleaningPhotoType.set()
+
+
 # Когда всё будет готово:
 
 @dp.message_handler(lambda message: message.text == "Готово", state="*")
@@ -161,5 +270,27 @@ async def finish(message: types.Message, state: FSMContext):
     await message.answer("Данные отправлены!", reply_markup=ReplyKeyboardRemove())
 
 
+@dp.message_handler(lambda message: message.text == "Готово", state=Form.CleaningPhotoType)
+async def send_report(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+
+    # Отправляем сообщение с текстовыми данными в чат ID_1
+    text_data = f"""
+Сумма смены: {data.get('shift_sum')}
+Количество людей записанных на чайную церемонию: {data.get('tea_ceremony_count')}
+Причина (если есть): {data.get('tea_ceremony_reason')}
+"""
+    await bot.send_message(CHAT_ID_1, text_data)
+
+    # Отправляем все фото в чат ID_1
+    for key, file_id in data.items():
+        if key not in ["shift_sum", "tea_ceremony_count", "tea_ceremony_reason"]:
+            await bot.send_photo(CHAT_ID_1, file_id)
+
+    # Отправляем фотографию чеков в чат ID_2
+    await bot.send_photo(CHAT_ID_2, data.get("receipts_photo"))
+
+    await message.answer("Все данные успешно отправлены!")
+    await state.finish()
 
 
